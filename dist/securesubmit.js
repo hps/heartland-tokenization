fe8cd2f0fc43dc35af498b509e79a17ae359a0a0
Shane Logsdon <shane.logsdon@e-hps.com>
2015-10-07 15:27:58 -0400

    update urls

--- dist/securesubmit.js
+++ dist/securesubmit.js
@@ -654,9 +654,9 @@ var Heartland;
 (function (Heartland) {
     Heartland.urls = {
         CERT: 'https://posgateway.cert.secureexchange.net/Hps.Exchange.PosGateway.Hpf.v1/api/token',
-        PROD: 'https://api2.heartlandportico.com/SecureSubmit.v1/api/token',
-        iframeCERT: 'http://192.168.39.1:8889/',
-        iframePROD: 'https://api2.heartlandportico.com/SecureSubmit.v1/token/2.0/'
+        PROD: 'https://api.heartlandportico.com/SecureSubmit.v1/api/token',
+        iframeCERT: 'https://hps.github.io/token/2.1/',
+        iframePROD: 'https://api.heartlandportico.com/SecureSubmit.v1/token/2.1/'
     };
 })(Heartland || (Heartland = {}));
 /// <reference path="../types/CardType.ts" />
--- src/vars/urls.ts
+++ src/vars/urls.ts
@@ -1,8 +1,8 @@
 module Heartland {
   export var urls = {
     CERT: 'https://posgateway.cert.secureexchange.net/Hps.Exchange.PosGateway.Hpf.v1/api/token',
-    PROD: 'https://api2.heartlandportico.com/SecureSubmit.v1/api/token',
-    iframeCERT: 'http://192.168.39.1:8889/',
-    iframePROD: 'https://api2.heartlandportico.com/SecureSubmit.v1/token/2.0/'
+    PROD: 'https://api.heartlandportico.com/SecureSubmit.v1/api/token',
+    iframeCERT: 'https://hps.github.io/token/2.1/',
+    iframePROD: 'https://api.heartlandportico.com/SecureSubmit.v1/token/2.1/'
   };
 }
