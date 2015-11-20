6c483386421ede7b5e64e7f3d785b15251679fe5
Chris Koerner <chris.koerner@e-hps.com>
2015-11-20 15:08:43 -0500

    update-cert-url

--- dist/securesubmit.js
+++ dist/securesubmit.js
@@ -783,7 +783,7 @@ var Heartland;
 var Heartland;
 (function (Heartland) {
     Heartland.urls = {
-        CERT: 'https://posgateway.cert.secureexchange.net/Hps.Exchange.PosGateway.Hpf.v1/api/token',
+        CERT: 'https://cert.api2.heartlandportico.com/Hps.Exchange.PosGateway.Hpf.v1/api/token',
         PROD: 'https://api.heartlandportico.com/SecureSubmit.v1/api/token',
         iframeCERT: 'https://hps.github.io/token/2.1/',
         iframePROD: 'https://api.heartlandportico.com/SecureSubmit.v1/token/2.1/'
