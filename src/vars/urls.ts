d4bf8f61de57878dc057e5d8579354ad01343522
Chris Koerner <chessmess@gmail.com>
2015-12-03 14:14:21 -0500

    Update urls.ts

--- src/vars/urls.ts
+++ src/vars/urls.ts
@@ -1,8 +1,8 @@
 module Heartland {
   export var urls = {
     CERT: 'https://cert.api2.heartlandportico.com/Hps.Exchange.PosGateway.Hpf.v1/api/token',
-    PROD: 'https://api.heartlandportico.com/SecureSubmit.v1/api/token',
+    PROD: 'https://api2.heartlandportico.com/SecureSubmit.v1/api/token',
     iframeCERT: 'https://hps.github.io/token/2.1/',
-    iframePROD: 'https://api.heartlandportico.com/SecureSubmit.v1/token/2.1/'
+    iframePROD: 'https://api2.heartlandportico.com/SecureSubmit.v1/token/2.1/'
   };
 }
