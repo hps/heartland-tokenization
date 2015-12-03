12a1fdaffe74ba24f100ee2d891033dd5c08fe3e
Chris Koerner <chessmess@gmail.com>
2015-12-03 14:16:23 -0500

    Update securesubmit.js

--- dist/securesubmit.js
+++ dist/securesubmit.js
@@ -784,9 +784,9 @@ var Heartland;
 (function (Heartland) {
     Heartland.urls = {
         CERT: 'https://cert.api2.heartlandportico.com/Hps.Exchange.PosGateway.Hpf.v1/api/token',
-        PROD: 'https://api.heartlandportico.com/SecureSubmit.v1/api/token',
+        PROD: 'https://api2.heartlandportico.com/SecureSubmit.v1/api/token',
         iframeCERT: 'https://hps.github.io/token/2.1/',
-        iframePROD: 'https://api.heartlandportico.com/SecureSubmit.v1/token/2.1/'
+        iframePROD: 'https://api2.heartlandportico.com/SecureSubmit.v1/token/2.1/'
     };
 })(Heartland || (Heartland = {}));
 /// <reference path="../types/CardType.ts" />
