690afe3daa43427673509a26e6d143c7265f0ee6
Shane Logsdon <shane@shanelogsdon.com>
2016-10-17 10:54:35 -0400

    update iframeCERT url

--- dist/securesubmit.js
+++ dist/securesubmit.js
@@ -1670,7 +1670,7 @@ var fields = [
 var urls = {
     CERT: 'https://cert.api2.heartlandportico.com/Hps.Exchange.PosGateway.Hpf.v1/api/token',
     PROD: 'https://api.heartlandportico.com/SecureSubmit.v1/api/token',
-    iframeCERT: 'http://localhost:7778/dist/',
+    iframeCERT: 'https://hps.github.io/token/2.1/',
     iframePROD: 'https://api.heartlandportico.com/SecureSubmit.v1/token/2.1/'
 };
 
--- src/vars/urls.ts
+++ src/vars/urls.ts
@@ -1,6 +1,6 @@
 export const urls = {
   CERT: 'https://cert.api2.heartlandportico.com/Hps.Exchange.PosGateway.Hpf.v1/api/token',
   PROD: 'https://api.heartlandportico.com/SecureSubmit.v1/api/token',
-  iframeCERT: 'http://localhost:7778/dist/', // 'https://hps.github.io/token/2.1/',
+  iframeCERT: 'https://hps.github.io/token/2.1/',
   iframePROD: 'https://api.heartlandportico.com/SecureSubmit.v1/token/2.1/'
 };
