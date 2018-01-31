2e29ff6d5160f8fa589d8834f2b1d2d0b182cffc
Shane Logsdon <shane@shanelogsdon.com>
2018-01-31 10:07:44 -0500

    define process for incrementing version numbers

---
+++ src/vars/version.ts
@@ -0,0 +1,14 @@
+/**
+ * WARNING: This module is updated with the project's
+ * `bin/update-version.js` script located at the root
+ * of the project. If you need to increment the project's
+ * version, do so in the project's `package.json` file.
+ *
+ * This version number should match what is listed as the
+ * `version` property in `package.json` as well as the
+ * deployed directory on `hps.github.io` in certification
+ * and `api2.heartlandportico.com` in production, e.g.:
+ *
+ *     https://hps.github.io/token/2.2.0/
+ */
+export default "2.2.0";
